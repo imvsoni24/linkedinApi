8d3a3c93d5b650bfdf022eb5c1580fa373b8aff9
imvsoni24 <imvsoni24@gmail.com>
2023-04-13 21:54:42 +0530

    back

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
-const express = require("express")
-require("dotenv").config()
-const {connection} = require("./configs/db");
+const express = require("express");
+require("dotenv").config();
+const { connection } = require("./configs/db");
 const cors = require("cors");
-const {userRouter}=require("./routes/user.route")
-const {postRouter} = require("./routes/post.route")
-const {auth} = require("./middlewares/auth.middleware")
+const { userRouter } = require("./routes/user.route");
+const { postRouter } = require("./routes/post.route");
+const { auth } = require("./middlewares/auth.middleware");
 const app = express();
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.send("Welcome to Linkedin")
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to Linkedin");
+});
 
-app.use("/users",userRouter)
+app.use("/users", userRouter);
 app.use(auth);
-app.use("/posts",postRouter)
+app.use("/posts", postRouter);
 
-
-
-
-
-app.listen(process.env.port,async()=>{
-    try{
-        await connection;
-        console.log("Server is running and connected to db");
-
-    }catch(err){
-        console.log(err)
-    }
-})
+app.listen(process.env.port, async () => {
+  try {
+    await connection;
+    console.log("Server is running and connected to db");
+  } catch (err) {
+    console.log(err);
+  }
+});
